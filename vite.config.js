39b1fa6ba79ed42e4a9258c3e61ef825f3accd07
Sulgie <niedermayr.s.m@gmail.com>
2024-12-26 15:58:15 +0100

    --

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/',
   plugins: [
     vue({
       template: { transformAssetUrls }
